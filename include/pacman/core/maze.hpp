2d9c5403c9e8d276c85b5ad5f424e9f99d4954c1
Nick Le Large <nick.lelarge@kit.edu>
2024-05-14 13:08:46 +0200

    Add function to build custom defined mazes
    Move some functions into the header (necessary due to function
    templates)

--- include/pacman/core/maze.hpp
+++ include/pacman/core/maze.hpp
@@ -11,17 +11,49 @@
 
 #include <cstdint>
 #include "util/grid.hpp"
+#include "util/pos.hpp"
 
-enum class Tile : std::uint8_t {
-  empty,
-  dot,
-  energizer,
-  wall,
-  door
-};
+enum class Tile : std::uint8_t { empty, dot, energizer, wall, door };
 
 using MazeState = Grid<Tile>;
 
+namespace {
+
+Tile stateChar(const char c) {
+    switch (c) {
+    case ' ':
+        return Tile::empty;
+    case '.':
+        return Tile::dot;
+    case 'o':
+        return Tile::energizer;
+    case '#':
+        return Tile::wall;
+    case '-':
+        return Tile::door;
+    default:
+        assert(false);
+        return {};
+    }
+}
+
+template <std::size_t Size>
+void stateStr(MazeState& state, const char (&str)[Size]) {
+    assert(state.area() + 1 == Size);
+    for (std::size_t i = 0; i != Size - 1; ++i) {
+        state[i] = stateChar(str[i]);
+    }
+}
+
+} // namespace
+
 MazeState makeMazeState();
+
+template <std::size_t Size>
+MazeState makeCustomMazeState(const Pos& size, const char (&str)[Size]) {
+    MazeState state{size};
+    stateStr(state, str);
+    return state;
+}
 
 #endif
